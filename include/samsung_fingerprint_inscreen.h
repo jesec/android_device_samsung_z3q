b44c7aa23ffb97248bd10674473421fd231ee86f
Jesse Chan <jc@lineageos.org>
2020-05-09 11:04:36 +0800

    x1q: configure inscreen fingerprint sensor
    Change-Id: Id8a1e091ab397a7b09b8ef9431507b1af4090d95
    Signed-off-by: Jesse Chan <jc@lineageos.org>

---
+++ include/samsung_fingerprint_inscreen.h
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2020 The LineageOS Project
+
+#ifndef SAMSUNG_FINGERPRINT_INSCREEN_H
+#define SAMSUNG_FINGERPRINT_INSCREEN_H
+
+#define FOD_SENSOR_X        554
+#define FOD_SENSOR_Y        2263
+#define FOD_SENSOR_SIZE     332
+
+#define FOD_ENABLE          "fod_enable,1,1"
+#define FOD_DISABLE         "fod_enable,0"
+
+#define FOD_SET_RECT        "set_fod_rect,554,2263,886,2595"
+
+#define FINGERPRINT_ACQUIRED_VENDOR 6
+#define VENDORCODE_FINGER_DOWN 9002
+#define VENDORCODE_FINGER_UP 9001
+
+#define TSP_CMD_PATH "/sys/class/sec/tsp/cmd"
+
+#endif  // SAMSUNG_FINGERPRINT_INSCREEN_H
